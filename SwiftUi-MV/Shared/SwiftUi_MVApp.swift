b2cb5e40f95a074af96607741e8f40a273af6f3e
Nermeen Tomoum <nrmeen.tomoum@alumni.fci-cu.edu.eg>
2022-06-07 11:17:17 +0400

    init

---
+++ SwiftUi-MV/Shared/SwiftUi_MVApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_MVApp.swift
+//  Shared
+//
+//  Created by Nrmeen Tomoum on 07/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_MVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
